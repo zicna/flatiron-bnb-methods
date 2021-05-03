e1f65d8248ba6817804131cdf0b98632f5b4e976
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-05-02 22:36:09 -0500

    commit tree 7f71489acbb5a9e8c442675f767612d8cb687dd5

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,20 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(date1, date2)
+    openings = []
+    checkin = Date.parse date1
+    checkout = Date.parse date2
+
+    self.listings.each do |listing|
+      listing.reservations.each do |reservation|
+        if reservation.checkin >= checkout || reservation.checkout <= checkin
+            openings << listing
+        end
+      end
+    end
+    openings
+  end
+
 end
 
